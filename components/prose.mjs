924e82e70ab9815565f257bbc700e6fc62c1d817
iliana etaoin <iliana@buttslol.net>
2022-04-01 00:26:54 -0700

    tailwind nit

--- components/prose.mjs
+++ components/prose.mjs
@@ -3,7 +3,7 @@ import { html } from "htm/preact";
 export default function Prose({ className, noMargins, children }) {
   return html`
     <div
-      className=${`prose dark:prose-invert prose-a:text-purple-600 prose-a:dark:text-purple-400 prose-a:font-medium lg:prose-lg xl:prose-xl 2xl:prose-2xl ${
+      className=${`prose dark:prose-invert prose-a:text-purple-600 dark:prose-a:text-purple-400 prose-a:font-medium lg:prose-lg xl:prose-xl 2xl:prose-2xl ${
         noMargins ? "" : "my-5 lg:my-6 xl:my-7 2xl:my-8"
       } ${className ?? ""}`.trim()}
     >
