c5961c7716b4d7f51238aab88a8238ddb51b7e45
iliana etaoin <iliana@buttslol.net>
2022-04-02 20:13:22 -0700

    add dingus

--- components/document.mjs
+++ components/document.mjs
@@ -77,6 +77,7 @@ export default function Base({ children }) {
             button.classList.remove("hidden");
           }}
         />
+        <script src="https://unreal-tea.iliana.fyi/script.js" data-site="APVSDETU" defer></script>
       </body>
     </html>
   `;
