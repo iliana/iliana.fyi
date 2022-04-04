c399a09bad7b6b1b0e925e2d7ffc5f27de4a934d
iliana etaoin <iliana@buttslol.net>
2022-04-03 19:04:30 -0700

    font shenanigans

--- fonts/fetch.sh
+++ fonts/fetch.sh
@@ -11,7 +11,7 @@ cd "${BASH_SOURCE%/*}/.."
 
 pushd fonts
 fetch https://github.com/rsms/inter/releases/download/v3.19/Inter-3.19.zip
-fetch https://github.com/be5invis/Iosevka/releases/download/v15.1.0/webfont-iosevka-15.1.0.zip
+fetch https://github.com/be5invis/Iosevka/releases/download/v15.1.0/webfont-iosevka-fixed-15.1.0.zip
 unzip -jo Inter-3.19.zip "Inter Web"/Inter-{roman,italic}.var.woff2
-unzip -jo webfont-iosevka-15.1.0.zip woff2/iosevka-extended{,bold}{,italic}.woff2
+unzip -jo webfont-iosevka-fixed-15.1.0.zip woff2/iosevka-fixed-extended{,bold}{,italic}.woff2
 popd
--- fonts/subset.sh
+++ fonts/subset.sh
@@ -9,9 +9,9 @@ unicodes=$(find . -type f '(' -name '*.html' -or -name '*.css' ')' -print0 \
 
 for f in *.woff2; do
     echo "$f"
-    [[ $f == Inter* ]] && features="calt,dnom,frac,locl,numr,pnum,tnum,kern,case,ss03"
-    [[ $f == iosevka* ]] && features="calt,locl,ccmp"
+    [[ $f == Inter* ]] && features="kern,case,ss03,cv05,cv08"
+    [[ $f == iosevka* ]] && features=""
     pyftsubset "$f" --flavor=woff2 \
-        --unicodes="U+0020-007F,$unicodes" --layout-features="$features"
+        --unicodes="U+0020-007F,U+2018-2019,U+201C-201D,$unicodes" --layout-features="$features"
     mv "${f%.woff2}.subset.woff2" "$f"
 done
