3f60040cc60ab57dab3112e742d7a5e4d49cc647
iliana etaoin <iliana@buttslol.net>
2022-08-06 21:48:08 -0700

    better multi-theme syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,19 @@
+// Copyright 2022 iliana etaoin
+// SPDX-License-Identifier: MIT-0
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify,
+// merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 const fs = require("fs");
 const path = require("path");
 const Image = require("@11ty/eleventy-img");
@@ -5,6 +21,97 @@ const dayjs = require("dayjs");
 const htmlmin = require("html-minifier");
 const { optimize } = require("svgo");
 const resolveConfig = require("tailwindcss/resolveConfig");
+
+function rehypeMultiShikiDrifting({ themes }) {
+  const highlightersPromise = import("shiki").then(({ getHighlighter }) =>
+    Promise.all(
+      Object.entries(themes).map(async ([label, theme]) => ({
+        label,
+        highlighter: await getHighlighter({ theme }),
+      }))
+    )
+  );
+
+  return async (tree) => {
+    const { visit } = await import("unist-util-visit");
+    const highlighters = await highlightersPromise;
+    visit(
+      tree,
+      // filter
+      (node) =>
+        node.type === "element" &&
+        node.tagName === "pre" &&
+        node.children.length === 1 &&
+        node.children[0].type === "element" &&
+        node.children[0].tagName === "code" &&
+        node.children[0].properties.className &&
+        node.children[0].properties.className.some((c) => c.startsWith("language-")) &&
+        node.children[0].children.length === 1 &&
+        node.children[0].children[0].type === "text",
+      // map
+      (node) => {
+        const code = node.children[0].children[0].value;
+        const language = node.children[0].properties.className
+          .find((c) => c.startsWith("language-"))
+          .slice("language-".length);
+        const tokenized = [];
+        highlighters.forEach(({ label, highlighter }) => {
+          highlighter.codeToThemedTokens(code, language).forEach((line, i) => {
+            if (!tokenized[i]) {
+              tokenized[i] = { content: line.map(({ content }) => content).join(""), themes: {} };
+            }
+            let pos = 0;
+            tokenized[i].themes[label] = line.map((token) => {
+              const newToken = { pos, ...token };
+              pos += token.content.length;
+              return newToken;
+            });
+          });
+        });
+        // eslint-disable-next-line no-param-reassign
+        node.properties.dataSyntaxHighlighted = true;
+        // eslint-disable-next-line no-param-reassign, no-shadow
+        node.children[0].children = tokenized.flatMap(({ content, themes }) => {
+          const starts = [
+            ...new Set(Object.entries(themes).flatMap(([, tokens]) => tokens.map(({ pos }) => pos))),
+          ].sort();
+          const children = starts.map((pos, i) => {
+            const styles = [];
+            Object.entries(themes).forEach(([label, tokens]) => {
+              const token = tokens
+                .slice()
+                .reverse()
+                .find((t) => t.pos <= pos);
+              styles.push(`--hl-color-${label}:${token.color}`);
+              // eslint-disable-next-line no-bitwise
+              if (token.fontStyle & 1) {
+                styles.push(`--hl-italic-${label}:italic`);
+              }
+              // eslint-disable-next-line no-bitwise
+              if (token.fontStyle & 2) {
+                styles.push(`--hl-bold-${label}:bold`);
+              }
+              // eslint-disable-next-line no-bitwise
+              if (token.fontStyle & 4) {
+                styles.push(`--hl-underline-${label}:underline`);
+              }
+            });
+            styles.sort();
+            return {
+              type: "element",
+              tagName: "span",
+              properties: { style: styles.join(";") },
+              children: [{ type: "text", value: content.slice(pos, starts[i + 1]) }],
+            };
+          });
+          children.push({ type: "text", value: "\n" });
+          return children;
+        });
+        node.children[0].children.pop();
+      }
+    );
+  };
+}
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy({ dist: "dist" });
@@ -20,12 +127,7 @@ module.exports = (eleventyConfig) => {
         .use((await import("remark-gfm")).default)
         .use((await import("remark-rehype")).default, { allowDangerousHtml: true })
         .use((await import("rehype-raw")).default)
-        .use((await import("rehype-pretty-code")).default, {
-          theme: {
-            dark: "github-dark",
-            light: "github-light",
-          },
-        })
+        .use(rehypeMultiShikiDrifting, { themes: { dark: "github-dark", light: "github-light" } })
         .use((await import("rehype-stringify")).default);
     },
     compile: (input) => async () => this.pipeline.process(input),
@@ -66,7 +168,7 @@ module.exports = (eleventyConfig) => {
     });
   });
 
-  // date formatting filter, similar to tera
+  // date formatting filter
   eleventyConfig.addNunjucksFilter("date", (value, formatString) =>
     dayjs(value).format(formatString ?? "YYYY-MM-DDTHH:mm:ssZ")
   );
